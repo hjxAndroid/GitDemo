1de5fad15abee63ef3894f3c8dc6b505199dea87
黄吉祥 <715108965@qq.com>
2016-03-22 14:02:54 +0800

    1

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -16,7 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    int a = 5;
+    int b = 10;
+    
+    int sum = a + b;
 }
 
 - (void)didReceiveMemoryWarning {
